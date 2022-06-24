a247bc86e749924d5d616171f5c1bf3a83f81ff0
Hrithik B <86526347+Hrithik1702@users.noreply.github.com>
2022-06-25 00:46:44 +0530

    Update sample.c

--- sample c file/sample.c
+++ sample c file/sample.c
@@ -1,1 +1,26 @@
+#include<stdlib.h>
+#include<stdio.h>
+#include<string.h>
+int main(){
+int l = 0;
+int n ;
+int j=;
+scanf("%d",&n);
+char str[n] ;
+scanf("%s",str);
+char temp[n] ;
+int k ;
+for(int i = 0 ; i < n ; i++){
+k = n-i-1 ;
+temp[i] = str[k];
+}
+int result = strcmp(str,temp); 
+if(result == 0){
+printf("Yes it is a palindrome");
+}
+else{
+printf("No it is not a palindrome");
+}
+}
+
 
